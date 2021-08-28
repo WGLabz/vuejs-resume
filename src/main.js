b786e14053a38d78882c63c8ce041b854610e544
Bikash Narayan Panda <oksbwn@gmail.com>
2021-08-28 20:16:01 +0100

    Minor Change

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from "./store";
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
