e530c18aab48ed9106490c1a2efb3f9017bccb9e
Bikash Narayan Panda <oksbwn@gmail.com>
2021-09-03 04:52:27 +0100

    Added Antd

--- src/components/ExperienceCard.vue
+++ src/components/ExperienceCard.vue
@@ -2,7 +2,36 @@
   <v-timeline-item color="pink" small>
     <v-card elevation="2" class="mx-auto mb-2">
       <v-card-title>{{ data.org }} </v-card-title>
-      <v-card-text class="pb-0"> </v-card-text>
+      <v-card-text class="pb-0">
+        <a-timeline>
+          <a-timeline-item color="green">
+            Create a services site 2015-09-01
+          </a-timeline-item>
+          <a-timeline-item color="green">
+            Create a services site 2015-09-01
+          </a-timeline-item>
+          <a-timeline-item color="red">
+            <p>Solve initial network problems 1</p>
+            <p>Solve initial network problems 2</p>
+            <p>Solve initial network problems 3 2015-09-01</p>
+          </a-timeline-item>
+          <a-timeline-item>
+            <p>Technical testing 1</p>
+            <p>Technical testing 2</p>
+            <p>Technical testing 3 2015-09-01</p>
+          </a-timeline-item>
+          <a-timeline-item color="gray">
+            <p>Technical testing 1</p>
+            <p>Technical testing 2</p>
+            <p>Technical testing 3 2015-09-01</p>
+          </a-timeline-item>
+          <a-timeline-item color="gray">
+            <p>Technical testing 1</p>
+            <p>Technical testing 2</p>
+            <p>Technical testing 3 2015-09-01</p>
+          </a-timeline-item>
+        </a-timeline>
+      </v-card-text>
     </v-card>
   </v-timeline-item>
 </template>
@@ -14,7 +43,6 @@ export default {
     data: {},
   },
   components: {},
-  data() {},
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from "./store";
 import router from './routes.js'
 import VueMeta from 'vue-meta'
 
+// Imports fot Ant Components
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(Antd);
+//End
+
 Vue.use(VueMeta)
 Vue.use(require('vue-moment'));
 
