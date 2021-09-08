1dae20c49cf04e2d5d0f61a7f06a6aaed3c27535
bikash_p <bikashnarayan.p@itcinfotech.com>
2021-09-08 23:49:07 +0530

    Refactore store actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,111 +1,63 @@
 import firebase from "../firebase/index";
 import { collection, getDocs } from "firebase/firestore";
 
+
+const firestoreCall = async (collection_) => {
+    const data = [];
+    const querySnapshot = await getDocs(collection(firebase.db, collection_));
+    querySnapshot.forEach((doc) => {
+        data.push(doc.data());
+    });
+    if (data.length === 0)
+        throw new Error("Please setup firebase " + collection_ + " collection.");
+    return data;
+}
+
+
 export default {
     async setUserMetaData(state) {
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "meta"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase meta collection.");
+        var data = await firestoreCall("meta");
         state.commit("setUserMetaData", data[0]);
     },
     async setSkills(state) {
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "skills"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase skills collection.");
-
+        var data = await firestoreCall("skills");
         state.commit("setSkills", data[0]);
     },
     async setTechStacks(state) {
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "tech"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase tech collection.");
+        var data = await firestoreCall("tech");
         state.commit("setTechStacks", data);
     },
     async setProjectsData(state) {
-        console.info('Fetching projects data.')
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "projects"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase projects collection.");
+        var data = await firestoreCall("projects");
         data.sort((x, y) => {
             return y.from.seconds - x.from.seconds
         })
         state.commit("setProjectsData", data);
     },
     async setEducationData(state) {
-        console.info('Fetching Education data.')
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "education"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase education collection.");
+        var data = await firestoreCall("education");
         data.sort((x, y) => {
             return y.from.seconds - x.from.seconds
         })
         state.commit("setEducationData", data);
     },
     async setCertificationsData(state) {
-        console.info('Fetching certifications data.')
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "certifications"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase certifications collection.");
+        var data = await firestoreCall("certifications");
         state.commit("setCertificationsData", data);
     },
     async setExperienceData(state) {
-        console.info('Fetching experience data.')
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "experience"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase experience collection.");
+        var data = await firestoreCall("experience");
         data.sort((x, y) => {
             return y.from.seconds - x.from.seconds
         })
         state.commit("setExperienceData", data);
     },
     async setPublicationsData(state) {
-        console.info('Fetching publications data.')
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "publications"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase p collection.");
+        var data = await firestoreCall("publications");
         state.commit("setPublicationsData", data);
     },
     async setContactDetails(state) {
-        console.info('Fetching publications data.')
-        const data = [];
-        const querySnapshot = await getDocs(collection(firebase.db, "contact"));
-        querySnapshot.forEach((doc) => {
-            data.push(doc.data());
-        });
-        if (data.length === 0)
-            throw new Error("Please setup firebase contact collection.");
+        var data = await firestoreCall("contact");
         state.commit("setContactDetails", data[0]);
     }
 }
