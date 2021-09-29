75fccac8ef87d5946f97fbc6e3b4e23fbda2ad66
bikash_p <bikashnarayan.p@itcinfotech.com>
2021-09-29 23:47:48 +0530

    jlt

---
+++ src/views/pages/Admin.vue
@@ -0,0 +1,33 @@
+<template>
+  <vue-json-editor
+    v-model="json"
+    :show-btns="true"
+    :expandedOnStart="true"
+    @json-change="onJsonChange"
+  ></vue-json-editor>
+</template>
+
+<script>
+import vueJsonEditor from "vue-json-editor";
+export default {
+  components: {
+    vueJsonEditor,
+  },
+  name: "Skills",
+  data() {
+    return {
+      json: {
+        msg: "demo of jsoneditor",
+      },
+    };
+  },
+  methods: {
+    onJsonChange(value) {
+      console.log("value:", value);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
